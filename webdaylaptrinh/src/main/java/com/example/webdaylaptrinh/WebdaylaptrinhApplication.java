b4349514b8b94f4629d7c272f265b58288ae1d18
thedat03 <nguyenthedat003@gmail.com>
2025-10-14 08:51:09 +0700

    First commit

---
+++ webdaylaptrinh/src/main/java/com/example/webdaylaptrinh/WebdaylaptrinhApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdaylaptrinh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdaylaptrinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdaylaptrinhApplication.class, args);
+	}
+
+}
---
+++ webdaylaptrinh/src/test/java/com/example/webdaylaptrinh/WebdaylaptrinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdaylaptrinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdaylaptrinhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
